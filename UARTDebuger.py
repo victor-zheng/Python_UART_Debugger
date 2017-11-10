f9fdfb61bc9ad8f97c4c1c1c407baac3ab590347
yi <yi@surface>
2017-11-11 07:57:41 +0800

    add com search function

--- UARTDebuger.py
+++ UARTDebuger.py
@@ -1,7 +1,7 @@
 import sys#系统相关的信息模块
 
 import serial
-
+import serial.tools.list_ports
 from PyQt5 import QtWidgets, QtCore, QtSerialPort
 from PyQt5.QtCore import QCoreApplication
 
@@ -11,14 +11,23 @@ class mywindow(QtWidgets.QMainWindow,Ui_MainWindow):#继承QWidget
     def __init__(self):
         super(mywindow,self).__init__()
         self.setupUi(self)
-        self.comboBox_port.mysignal.connect(self.rxHandle)
+        self.comboBox_port.mysignal.connect(self.pushButton_openport_Handle)
         self.pushButton_openport.clicked.connect(self.ButtonHandle)
     def ButtonHandle(self):
         ser = serial.Serial('COM7')
         self.textBrowser_sysinf.setText(ser.name)
         ser.write(b'hello')
-    def rxHandle(self):
-        print("received signal")
+    def pushButton_openport_Handle(self):
+        port_list = list(serial.tools.list_ports.comports())
+        length = len(port_list)
+        if  length<= 0:
+            print("The Serial port can't find!")
+        else:
+            print(length)
+            port_list_0 =list(port_list[0])
+            port_serial = port_list_0[0]
+            ser = serial.Serial(port_serial,9600,timeout = 60)
+            print("check which port was really used >",ser.name)
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     myshow = mywindow()
