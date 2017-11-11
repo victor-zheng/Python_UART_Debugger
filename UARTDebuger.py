d66cb2e0a8109434b8d6219f91fecb13b0b8127a
yi <yi@surface>
2017-11-11 16:10:00 +0800

    1.comboBox_port ready

--- UARTDebuger.py
+++ UARTDebuger.py
@@ -11,13 +11,13 @@ class mywindow(QtWidgets.QMainWindow,Ui_MainWindow):#继承QWidget
     def __init__(self):
         super(mywindow,self).__init__()
         self.setupUi(self)
-        self.comboBox_port.mysignal.connect(self.pushButton_openport_Handle)
-        self.pushButton_openport.clicked.connect(self.ButtonHandle)
-    def ButtonHandle(self):
-        ser = serial.Serial('COM7')
-        self.textBrowser_sysinf.setText(ser.name)
-        ser.write(b'hello')
-    def pushButton_openport_Handle(self):
+        #initialize the UART parameter selection UI
+        self.comboBox_port.mysignal.connect(self.comboBox_port_DeviceSearch_Handle)
+        self.comboBox_port.activated.connect(self.comboBox_port_DeviceSelection_Handle)
+        
+        self.pushButton_openport.clicked.connect(self.pushButton_openport_Handle)
+
+    def comboBox_port_DeviceSearch_Handle(self):
         self.comboBox_port.clear()
         port_list = list(serial.tools.list_ports.comports())
         length = len(port_list)
@@ -28,6 +28,14 @@ class mywindow(QtWidgets.QMainWindow,Ui_MainWindow):#继承QWidget
                 port_temp = list(port_list[i])
                 print("check which port was really used >",port_temp[0])
                 self.comboBox_port.addItem(port_temp[0])
+    def comboBox_port_DeviceSelection_Handle(self):
+        ActivePortName = self.comboBox_port.currentText()
+        print("selected",ActivePortName)
+        
+    def pushButton_openport_Handle(self):
+        ser = serial.Serial('COM7')
+        self.textBrowser_sysinf.setText(ser.name)
+        ser.write(b'hello')
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     myshow = mywindow()
